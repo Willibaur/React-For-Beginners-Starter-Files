1c8f563ad9059d466fe870ae74eb1e36a8767643
willibaur <williambautista@gmail.com>
2019-02-03 19:31:32 +0000

    Handling event refs, binding and routing

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+
+    // 2. get the text from the input
+    const storeName = this.myInput.current.value;
+
+    // 3. change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+
+        <input
+          defaultValue={getFunName()}
+          placeholder="Store Name"
+          ref={this.myInput}
+          required
+          type="text"
+        />
+
         <button type="submit">Visit Store =></button>
       </form>
     );
